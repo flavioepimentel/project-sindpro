a038b5d66972e5ead077c9546fe9cdaf77c8b676
flavioepimentel <flavio.ericksan@hotmail.com>
2023-09-25 19:00:25 -0300

    build: :construction: Script de envio de e-mail - API não habilitada

--- Contact/script.js
+++ Contact/script.js
@@ -1,18 +1,6 @@
-// var addresses = "";//between the speech mark goes the receptient. Seperate addresses with a ;
-// var body = ""//write the message text between the speech marks or put a variable in the place of the speech marks
-// var subject = ""//between the speech marks goes the subject of the message
-// var href = "mailto:" + addresses + "?"
-//          + "subject=" + subject + "&"
-//          + "body=" + body;
-// var wndMail;
-// wndMail = window.open(href, "_blank", "scrollbars=yes,resizable=yes,width=10,height=10");
-// if(wndMail)
-// {
-//     wndMail.close();
-// }
+const API_LOCAWEB = "https://api.smtplw.com.br/v1/messages"
 
 
-const nodemailer = require('nodemailer')
 
 function getEmailSender() {
     return document.getElementById('email_sender').value
@@ -21,32 +9,34 @@ function getEmailSender() {
 function getMessage() {
     return document.getElementById('textarea_email').value
 }
-
-const html = `<h1>Hello Word</h1>`
-
-
-function sendEmail() {
-    const email = getEmailSender()
-    const mensage = getMessage()
-    return (email, mensage)
-}
-
-
-const API_LOCAWEB = "https://api.smtplw.com.br/v1/messages"
-// content-type: application/json
-/// x-auth-token: 7sd6g8f7sdg78fsdg7fsd
-
-const obj = {
+function emailConstruction (email, mensage) {
+    return {
     "subject": "Contato por WebSite", //"Titulo"
     "body": mensage, // Texto principal
     "from": email, //"remetente@dominio.com"
-    "to": "contato@sindpro.net", // "destinatario@dominio2.com"
+    "to": "flavio.ericksan@hotmail.com", // "destinatario@dominio2.com" | "contato@sindpro.net"
     "cc": [],
     "bcc": [],
     "headers": {
         "Content-Type": "text/plain"
     }
 }
+}
+
+function sendEmail() {
+    const email = getEmailSender()
+    const mensage = getMessage()
+    const emailBody = emailConstruction(email, mensage)
+    fetch(API_LOCAWEB, emailBody).then(function (response) {
+    console.log(response.blob());
+  })
+}
+
+
+
+// content-type: application/json
+/// x-auth-token: 7sd6g8f7sdg78fsdg7fsd
+
 
 
 // RESPOSTA:
